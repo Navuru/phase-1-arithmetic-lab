6c53e66375d2cb1093c6393be3232944fe649c04
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-08-19 14:09:30 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 62;
+let num2 = 1;
 
-// Write your code here
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 10);
+
+let num3 = 24
+let num4 = 5
+const mod = num3 % num4;
+
+const max = Math.max(5,3,7,20);
